6cf7f3cbf67b64eb5d163a4973b29ca1b4d3a356
jai ganesh <jaiganeshmothilal@gmail.com>
2023-07-27 19:05:03 +0530

    modified

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -170,7 +170,7 @@ const HomePage = () => {
                     </h5>
                   </div>
                   <p className="card-text ">
-                    {p.description.substring(0, 60)}...
+                    {p.description.substring(0, 40)}...
                   </p>
                   <div className="card-name-price">
                     <button
