04765f47d680c354800ff12f3a32248384434083
jaiganesh-23 <jaiganeshmothilal22@gmail.com>
2025-03-10 19:53:32 +0530

    modified product page

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -3,11 +3,14 @@ import Layout from "./../components/Layout/Layout";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import "../styles/ProductDetailsStyles.css";
+import { useCart } from "../context/cart";
+import { toast } from "react-hot-toast";
 
 const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState({});
+  const [cart, setCart] = useCart();
   const [relatedProducts, setRelatedProducts] = useState([]);
 
   //initalp details
@@ -62,7 +65,14 @@ const ProductDetails = () => {
             })}
           </h6>
           <h6>Category : {product?.category?.name}</h6>
-          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+          <button onClick={() => {
+                    setCart([...cart, product]);
+                    localStorage.setItem(
+                      "cart",
+                      JSON.stringify([...cart, product])
+                    );
+                    toast.success("Item Added to cart");
+                  }} class="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
       </div>
       <hr />
